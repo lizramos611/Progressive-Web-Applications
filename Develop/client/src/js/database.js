2dd3ab2d34451d82615af0de388a5ee2f285c1cd
lizramos611 <liz.ramos@aim.com>
2023-03-18 13:54:49 -0400

    adding in db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  tx.store.add({content, created_at: new Date()});
+  await tx.done;
+  console.error('putDb not implemented');
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const data = await tx.store.getAll();
+  console.log(data);
+  console.error('getDb not implemented');
+  return data;
+  
+}
 initdb();
